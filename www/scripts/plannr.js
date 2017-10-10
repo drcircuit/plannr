538928dc8b61fe12c0d571c61da42ee94d981730
espensl <espensl@compello.com>
2017-10-10 22:30:15 +0200

    fixed bug

--- www/scripts/plannr.js
+++ www/scripts/plannr.js
@@ -55,7 +55,7 @@ angular.module('plannr', ['ngDragDrop','ngStorage'])
             for (var i = 0; i < 24; i++) {
                 var clock = startClock + i;
                 if (clock > 23) {
-                    startClock = -16;
+                    startClock = -15;
                     clock      = 0;
                 }
                 $scope.$storage.schedule.push({
